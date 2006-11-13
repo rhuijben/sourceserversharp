4d8fd8e7402eb1865e3a46148f6bb61c3a308dbb
bhuijben <bhuijben@2e547e83-e221-0410-8eb0-7b0e83e86450>
2006-11-13 01:17:05 +0000

    Make subversion commands quiet

--- trunk/src/QQn.SourceServerIndexer/Providers/SubversionResolver.cs
+++ trunk/src/QQn.SourceServerIndexer/Providers/SubversionResolver.cs
@@ -205,7 +205,7 @@ namespace QQn.SourceServerIndexer.Providers
 			writer.Write(Id);
 			writer.Write("__CMD=svn.exe export \"%var3%%var4%@%var5%\" \"%");
 			writer.Write(Id);
-			writer.WriteLine("__TRG%\" --force --non-interactive --non-recursive");
+			writer.WriteLine("__TRG%\" --non-interactive --quiet");
 		}
 	}
 
